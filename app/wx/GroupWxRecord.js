c0c8cf91f154b892316d87869b9368ed5f2706e8
wzt <wangzt@qll-times.com>
2021-12-22 15:30:05 +0800

    add

--- app/wx/GroupWxRecord.js
+++ app/wx/GroupWxRecord.js
@@ -8,6 +8,11 @@ router.post('/group/record', async (ctx, next) => {
   let body = ctx.request.body
   if(!body.content) throw new global.customError.ServiceError('内容不能为空')
   GroupWxRecord.saveRecord(body.name, body.content)
+  ctx.status = 200
+  ctx.body = {
+    message: 'ok',
+    code: 0
+  }
 })
 
 module.exports = router

fb78a9aba0b244b0282c4b858c9e1f7956949a16
wzt <wangzt@qll-times.com>
2021-12-30 17:54:12 +0800

    add

---
+++ app/api/utils.js
@@ -0,0 +1,41 @@
+const Router = require('koa-router')
+const router = new Router({
+  prefix: '/utils'
+})
+
+router.post('/ticket/add', async (ctx, next) => {
+  let body = ctx.request.body
+  if(!body.name){
+    return ctx.body = {
+      message: '股票名称不能为空',
+      status: 1
+    }
+  }
+  if(!body.code){
+    return ctx.body = {
+      message: '股票代码不能为空',
+      status: 1
+    }
+  }
+  let fileAddress = '/usr/local/nodejs/projects/my-robot/config/code.js'
+  const fs = require('fs')
+  const buffer = fs.readFileSync(fileAddress)
+  let text = String(buffer)
+  text = text.replace('module.exports =', '')
+  text = JSON.parse(text)
+  text.push({
+    name: body.name,
+    content: body.code
+  })
+  text = JSON.stringify(text)
+  text = 'module.exports = ' + text
+  fs.writeFile(fileAddress, text, {flag:'w'}, () => {})
+  ctx.body = {
+    message: 'ok',
+    name: body.name,
+    content: body.code,
+    status: 0
+  }
+})
+
+module.exports = router

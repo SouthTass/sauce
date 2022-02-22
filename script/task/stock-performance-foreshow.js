cc44ea74dafca7f9043ad8f1e2e9228c618853f5
wzt <wangzt@qll-times.com>
2022-02-22 16:44:48 +0800

    add

--- script/task/stock-performance-foreshow.js
+++ script/task/stock-performance-foreshow.js
@@ -40,28 +40,34 @@ performance.performanceForecast = async function (){
 // 查询业绩预告(2022年一季报)
 performance.performanceForecast202201 = async function (){
   let body = {}
-  let res = await axios.get('https://data.10jqka.com.cn/ajax/yjyg/date/2022-03-31', { 
-    responseType: 'arraybuffer',
-    headers: {
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept-Language': 'zh-CN,zh;q=0.9',
-      'Cache-Control': 'max-age=0',
-      'Connection': 'keep-alive',
-      'Cookie': 'searchGuide=sg; Hm_lvt_f79b64788a4e377c608617fba4c736e2=1644918805,1645173346,1645430566; Hm_lvt_60bad21af9c824a4a0530d5dbf4357ca=1644918805,1645173346,1645430566; Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1645080172,1645173329,1645174075,1645430566; log=; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1645515717; Hm_lpvt_f79b64788a4e377c608617fba4c736e2=1645515717; Hm_lpvt_60bad21af9c824a4a0530d5dbf4357ca=1645515717; v=A-IClYN89ClcR-h1yr3NBbUvNWNBM-ZNmDfacSx7DtUA_4zdFMM2XWjHKoD_',
-      'Host': 'data.10jqka.com.cn',
-      'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': "macOS",
-      'Sec-Fetch-Dest': 'document',
-      'Sec-Fetch-Mode': 'navigate',
-      'Sec-Fetch-Site': 'none',
-      'Sec-Fetch-User': '?1',
-      'Upgrade-Insecure-Requests': '1',
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
-      'hexin-v': 'A98_Lq6rwYKwycWe-CuYfkBwaDhsRDQOTZA3xHEseVbVGPEmeRTDNl1oxyOC',
-    }
-  })
+  let res
+  try{
+    res = await axios.get('https://data.10jqka.com.cn/ajax/yjyg/date/2022-03-31', { 
+      responseType: 'arraybuffer',
+      headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Cookie': 'searchGuide=sg; Hm_lvt_f79b64788a4e377c608617fba4c736e2=1644918805,1645173346,1645430566; Hm_lvt_60bad21af9c824a4a0530d5dbf4357ca=1644918805,1645173346,1645430566; Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1645080172,1645173329,1645174075,1645430566; log=; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1645515717; Hm_lpvt_f79b64788a4e377c608617fba4c736e2=1645515717; Hm_lpvt_60bad21af9c824a4a0530d5dbf4357ca=1645515717; v=A-IClYN89ClcR-h1yr3NBbUvNWNBM-ZNmDfacSx7DtUA_4zdFMM2XWjHKoD_',
+        'Host': 'data.10jqka.com.cn',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': "macOS",
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
+        'hexin-v': 'A98_Lq6rwYKwycWe-CuYfkBwaDhsRDQOTZA3xHEseVbVGPEmeRTDNl1oxyOC',
+      }
+    })
+  }catch(error){
+    console.log(`请求错误====>${error.response.status}`)
+  }
+  
   if(!res) return
   let str = iconv.decode(Buffer.from(res.data), 'gb2312')
   let html = iconv.encode(str, 'utf8').toString()
--- test1.js
+++ test1.js
@@ -6,12 +6,18 @@ const iconv = require('iconv-lite')
 // 查询业绩预告(2022年一季报)
 async function tests (){
   let body = {}
-  let res = await axios.get('https://data.10jqka.com.cn/ajax/yjyg/date/2022-03-31', { 
-    responseType: 'arraybuffer',
-    headers: {
-      'Cookie': 'searchGuide=sg; Hm_lvt_f79b64788a4e377c608617fba4c736e2=1644918805,1645173346,1645430566; Hm_lvt_60bad21af9c824a4a0530d5dbf4357ca=1644918805,1645173346,1645430566; Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1645080172,1645173329,1645174075,1645430566; log=; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1645515717; Hm_lpvt_f79b64788a4e377c608617fba4c736e2=1645515717; Hm_lpvt_60bad21af9c824a4a0530d5dbf4357ca=1645515717; v=A-IClYN89ClcR-h1yr3NBbUvNWNBM-ZNmDfacSx7DtUA_4zdFMM2XWjHKoD_',
-    }
-  })
+  try {
+    let res = await axios.get('https://data.10jqka.com.cn/ajax/yjyg/date/2022-03-31', { 
+      responseType: 'arraybuffer'
+    })
+  } catch (error) {
+    console.log(1, error.response.status)
+    // let str = iconv.decode(Buffer.from(error.response.data), 'gb2312')
+    // let html = iconv.encode(str, 'utf8').toString()
+    // console.log(html)
+  }
+  
+  return
   if(!res) return
   let str = iconv.decode(Buffer.from(res.data), 'gb2312')
   let html = iconv.encode(str, 'utf8').toString()

8dd4513b864c2b099f75d69007e82fc74ea30558
wzt <wangzt@qll-times.com>
2023-04-13 10:21:51 +0800

    调整推送为60秒

--- script/task/index.js
+++ script/task/index.js
@@ -4,14 +4,22 @@ let task = {}
 // 初始化任务
 task.init = function init(){
   setIntervalTaskFiveSecond()
+  setIntervalTaskSixtySecond()
 }
 
 // 每隔5秒去执行的定时任务
 async function setIntervalTaskFiveSecond(){
   setInterval(() => {
+    
+  }, 5000);
+}
+
+// 每隔60秒去执行的定时任务
+async function setIntervalTaskSixtySecond(){
+  setInterval(() => {
     stockPerformanceForeshow.performanceForecast202203()
     stockPerformanceForeshow.performanceForecast202303()
-  }, 5000);
+  }, 60000);
 }
 
 module.exports = task

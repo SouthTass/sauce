74c2e5c5fcee208b5a108b1fc462a0602cd55a68
wzt <wangzt@qll-times.com>
2023-05-22 10:02:27 +0800

    增加schedule插件

--- script/task/index.js
+++ script/task/index.js
@@ -1,24 +1,33 @@
 const stockPerformanceForeshow = require('./stock-performance-foreshow')
+const schedule = require('node-schedule')
+const dayjs = require('dayjs')
 let task = {}
+//其他规则见 https://www.npmjs.com/package/node-schedule
+//规则参数讲解    *代表通配符
+// *  *  *  *  *  *
+// ┬ ┬ ┬ ┬ ┬ ┬
+// │ │ │ │ │  |
+// │ │ │ │ │ └ day of week (0 - 7) (0 or 7 is Sun)
+// │ │ │ │ └───── month (1 - 12)
+// │ │ │ └────────── day of month (1 - 31)
+// │ │ └─────────────── hour (0 - 23)
+// │ └──────────────────── minute (0 - 59)
+// └───────────────────────── second (0 - 59, OPTIONAL)
+// 每分钟的第30秒触发： '30 * * * * *'
+// 每小时的1分30秒触发 ：'30 1 * * * *'
+// 每天的凌晨1点1分30秒触发 ：'30 1 1 * * *'
+// 每月的1日1点1分30秒触发 ：'30 1 1 1 * *'
+// 每周1的1点1分30秒触发 ：'30 1 1 * * 1'
 
 // 初始化任务
 task.init = function init(){
   setIntervalTaskFiveSecond()
   setIntervalTaskSixtySecond()
-}
 
-// 每隔5秒去执行的定时任务
-async function setIntervalTaskFiveSecond(){
-  setInterval(() => {
-    // stockPerformanceForeshow.performanceTimeDayPrice()
-  }, 5000)
-}
-
-// 每隔60秒去执行的定时任务
-async function setIntervalTaskSixtySecond(){
-  setInterval(() => {
+  // 每分钟的第一秒执行
+  schedule.scheduleJob('01 * * * * *', () => {
     stockPerformanceForeshow.performanceForecast()
-  }, 60000)
+  })
 }
 
 module.exports = task

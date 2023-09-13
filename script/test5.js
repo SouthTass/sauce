6fd8702c38a773998adb829060a91687c41af796
王正涛 <wangzt@qll-times.com>
2023-09-13 10:54:14 +0800

    新增测试脚本

--- script/test5.js
+++ script/test5.js
@@ -1,41 +1,20 @@
 const axios = require('axios')
 let list = []
-const current = ['08', '10', '18', '23', '29', '03', '08']
+const current = ['06', '15', '16', '32', '33', '02', '07']
 async function getWarningFile(){
   let res
   try {
     res = await axios.get(`http://101.43.217.166:3000/micro/powerball/list?type=%E5%A4%A7%E4%B9%90%E9%80%8F`)
     list = res.data.list
-    handleResult()
-    handleRedRepetition()
     autoHandleResult()
+    autoHandleBlueResult()
+    handleResult()
+    handleBlueRepetition()
   } catch (error) {
     return console.log('调用出错:', JSON.stringify(error))
   }
 }
 getWarningFile()
-
-function handleResult(){
-  let num5 = current[5]
-  let num6 = current[6]
-  blueCount = 0
-  list.map(e => {
-    if((num5 == e.ball5 || num5 == e.ball6) && (num6 == e.ball5 || num6 == e.ball6)){
-      blueCount++
-    }
-  })
-  console.log(`您的号码篮球出现的次数为：${blueCount}`)
-}
-
-function handleRedRepetition(){
-  let count = 0
-  list.map(e => {
-    let sample = `${current[0]}${current[1]}${current[2]}${current[3]}${current[4]}`
-    let eSample = `${e.ball0}${e.ball1}${e.ball2}${e.ball3}${e.ball4}`
-    if(sample == eSample) count++
-  })
-  console.log(`您的号码红球完全重复的次数为：${count}`)
-}
 
 function autoHandleResult(){
   let blueList = []
@@ -57,4 +36,44 @@ function autoHandleResult(){
     return a.count - b.count
   })
   console.log('大乐透历届红球组及出现的次数：', blueList)
+}
+
+function autoHandleBlueResult(){
+  let data = JSON.parse(JSON.stringify(list))
+  let BList = []
+  data.map(e => {
+    BList.push({text: `${e.ball0}${e.ball1}${e.ball2}${e.ball3}${e.ball4}`, num: 0})
+  })
+  list.map(e => {
+    let text = `${e.ball0}${e.ball1}${e.ball2}${e.ball3}${e.ball4}`
+    let index = BList.findIndex(b => e.text == text)
+    if(index != -1) BList[index].num++
+  })
+  let result = []
+  BList.map(e => {
+    if(e.num > 1) result.push(e)
+  })
+  console.log(`历史数据中蓝球全部重复的为：`, result)
+}
+
+function handleResult(){
+  let num5 = current[5]
+  let num6 = current[6]
+  blueCount = 0
+  list.map(e => {
+    if((num5 == e.ball5 || num5 == e.ball6) && (num6 == e.ball5 || num6 == e.ball6)){
+      blueCount++
+    }
+  })
+  console.log(`您的号码黄球出现的次数为：${blueCount}`)
+}
+
+function handleBlueRepetition(){
+  let count = 0
+  list.map(e => {
+    let sample = `${current[0]}${current[1]}${current[2]}${current[3]}${current[4]}`
+    let eSample = `${e.ball0}${e.ball1}${e.ball2}${e.ball3}${e.ball4}`
+    if(sample == eSample) count++
+  })
+  console.log(`您的号码蓝球完全重复的次数为：${count}`)
 }

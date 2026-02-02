b2b1515a128424f8466cb9ef565cd299fd985e38
王正涛 <wangzt@qll-times.com>
2026-02-02 17:15:49 +0800

    增加xauusd接口

--- app/tools/Index.js
+++ app/tools/Index.js
@@ -1,4 +1,5 @@
 const dayjs = require('dayjs')
+const axios = require('axios')
 const Router = require('koa-router')
 const router = new Router({
   prefix: '/api/tools'
@@ -8,6 +9,27 @@ const router = new Router({
 router.get('/time/now', async (ctx, next) => {
   ctx.status = 200
   ctx.body = dayjs().format('YYYY-MM-DD HH:mm:ss')
+})
+
+// 获取国际黄金价格
+router.get('/price/xauusd', async (ctx, next) => {
+  let res = await axios({
+    url: 'https://api.jijinhao.com/sQuoteCenter/realTime.htm?code=JO_92233&_=1770013101589',
+    method: 'get',
+    headers: {
+      'User-Agent': 'JTW/6.21.0 (iPhone; iPhone11,8; 18.6; Scale/2.00)',
+      'Referer': 'https://quote.cngold.org/'
+    }
+  })
+  if(res.status != 200){
+    ctx.status = 200
+    ctx.body = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  }else{
+    eval(res.data)
+    let arr = hq_str.split(',')
+    ctx.status = 200
+    ctx.body = arr[3]
+  }
 })
 
 module.exports = router
